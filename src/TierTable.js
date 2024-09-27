ecca3b6696ce596a9e919baf86b4fdc4d4b452b6
Nick Yoon <89yunik@gmail.com>
2024-09-27 18:31:32 +0900

    fix : change tag id

--- src/TierTable.js
+++ src/TierTable.js
@@ -21,7 +21,7 @@ const TierTable = () => {
       rows[props.index][rowKey] = e.target.value
     }
     return (
-      <div className="tier-row">
+      <div className="tier-table-row">
         <AutosizeInput className="tier-name" type="text" value={tierName} onChange={changeTierName} />
         <span className="tier-images">
           <button className="add-tier-btn btn btn-link" onClick={() => addRow(props.index)}>
@@ -33,7 +33,7 @@ const TierTable = () => {
   }
 
   return (
-    <div>
+    <div id="tier-table">
       {Array.from(rows).map((row, i) => (
         <TierTableRow key={i} index={i} rowInfo={row} />
       ))}
