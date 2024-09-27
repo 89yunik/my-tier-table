529256bd0313a9b8282ef2a4b8377a1f0ee5ab98
Nick Yoon <89yunik@gmail.com>
2024-09-28 01:31:45 +0900

    feat : add drag & drop image for tier table

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Gallery from "./Gallery"
 
 function App() {
   return (
-    <div id="app">
+    <div id="app" onDragOver={(e) => e.preventDefault()}>
       <header id="app-header">
         <TierTable />
         <Gallery />
