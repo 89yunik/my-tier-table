4e6ad8faad61e04f49e83119611a1c628e8a28cd
Nick Yoon <89yunik@gmail.com>
2024-09-28 11:02:21 +0900

    refactor : delete title

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Gallery from "./Gallery"
 function App() {
   return (
     <div id="app" onDragOver={(e) => e.preventDefault()}>
-      <h1 id="app-title">My Tier Table</h1>
+      {/* <h1 id="app-title">My Tier Table</h1> */}
       <div id="app-body">
         <TierTable />
         <Gallery />
